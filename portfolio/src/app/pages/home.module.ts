946a2b6dc192efb32272baa097007c0368e8f4b0
Salim <salim.hassan.welleat@gmail.com>
2024-03-30 13:58:37 +0100

    build: app footer

---
+++ portfolio/src/app/pages/home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [],
+  declarations: [HomePage, HeaderComponent],
+})
+export class HomePageModule {}
