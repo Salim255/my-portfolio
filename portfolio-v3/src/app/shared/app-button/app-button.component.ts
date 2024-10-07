24c17f024186e5e98d8c62d5b0efc0ae85c9b389
Salim <salim.hassan.welleat@gmail.com>
2024-10-07 14:59:34 +0200

    built: call for action

--- portfolio-v3/src/app/shared/app-button/app-button.component.ts
+++ portfolio-v3/src/app/shared/app-button/app-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,6 @@ import { Component } from "@angular/core";
 })
 
 export class AppButtonComponent {
+  @Input() content!: string
     constructor(){}
 }
