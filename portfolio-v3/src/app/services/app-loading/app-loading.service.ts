b410acc95d61bf07e1217b89ebe76951c0ec3eae
Salim <salim.hassan.welleat@gmail.com>
2024-11-02 13:19:59 +0100

    added: app loading service

---
+++ portfolio-v3/src/app/services/app-loading/app-loading.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppLoadingService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  getLoading$ = this.loadingSubject.asObservable();
+
+  constructor(){}
+
+  show(){
+    this.loadingSubject.next(true);
+  }
+
+  hide(){
+    this.loadingSubject.next(false);
+  }
+}
